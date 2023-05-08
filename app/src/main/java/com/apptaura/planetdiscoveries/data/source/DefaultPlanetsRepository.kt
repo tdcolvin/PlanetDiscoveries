0d62cd209ca6ace883d9514ee3a2766e8d57449a
Tom Colvin <tdcolvin@gmail.com>
2023-05-08 19:26:23 +0100

    Added code for the repository

--- app/src/main/java/com/apptaura/planetdiscoveries/data/source/DefaultPlanetsRepository.kt
+++ app/src/main/java/com/apptaura/planetdiscoveries/data/source/DefaultPlanetsRepository.kt
@@ -15,8 +15,14 @@
  */
 package com.apptaura.planetdiscoveries.data.source
 
+import com.apptaura.planetdiscoveries.data.Planet
+import com.apptaura.planetdiscoveries.data.WorkResult
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Default implementation of [PlanetsRepository]. Single entry point for managing planets' data.
@@ -27,4 +33,85 @@ class DefaultPlanetsRepository(
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : PlanetsRepository {
 
+    override suspend fun getPlanets(forceUpdate: Boolean): WorkResult<List<Planet>> {
+        if (forceUpdate) {
+            try {
+                updatePlanetsFromRemoteDataSource()
+            } catch (ex: Exception) {
+                return WorkResult.Error(ex)
+            }
+        }
+        return planetsLocalDataSource.getPlanets()
+    }
+
+    override suspend fun refreshPlanets() {
+        updatePlanetsFromRemoteDataSource()
+    }
+
+    override fun getPlanetsStream(): Flow<WorkResult<List<Planet>>> {
+        return planetsLocalDataSource.getPlanetsStream()
+    }
+
+    override suspend fun refreshPlanet(planetId: String) {
+        updatePlanetFromRemoteDataSource(planetId)
+    }
+
+    private suspend fun updatePlanetsFromRemoteDataSource() {
+        val remotePlanets = planetsRemoteDataSource.getPlanets()
+
+        if (remotePlanets is WorkResult.Success) {
+            // Real apps might want to do a proper sync, deleting, modifying or adding each planet.
+            planetsLocalDataSource.deleteAllPlanets()
+            remotePlanets.data.forEach { planet ->
+                planetsLocalDataSource.savePlanet(planet)
+            }
+        } else if (remotePlanets is WorkResult.Error) {
+            throw remotePlanets.exception
+        }
+    }
+
+    override fun getPlanetStream(planetId: String): Flow<WorkResult<Planet>> {
+        return planetsLocalDataSource.getPlanetStream(planetId)
+    }
+
+    private suspend fun updatePlanetFromRemoteDataSource(planetId: String) {
+        val remotePlanet = planetsRemoteDataSource.getPlanet(planetId)
+
+        if (remotePlanet is WorkResult.Success) {
+            planetsLocalDataSource.savePlanet(remotePlanet.data)
+        }
+    }
+
+    /**
+     * Relies on [getPlanets] to fetch data and picks the planet with the same ID.
+     */
+    override suspend fun getPlanet(planetId: String, forceUpdate: Boolean): WorkResult<Planet> {
+        if (forceUpdate) {
+            updatePlanetFromRemoteDataSource(planetId)
+        }
+        return planetsLocalDataSource.getPlanet(planetId)
+    }
+
+    override suspend fun savePlanet(planet: Planet) {
+        coroutineScope {
+            launch { planetsRemoteDataSource.savePlanet(planet) }
+            launch { planetsLocalDataSource.savePlanet(planet) }
+        }
+    }
+
+    override suspend fun deleteAllPlanets() {
+        withContext(ioDispatcher) {
+            coroutineScope {
+                launch { planetsRemoteDataSource.deleteAllPlanets() }
+                launch { planetsLocalDataSource.deleteAllPlanets() }
+            }
+        }
+    }
+
+    override suspend fun deletePlanet(planetId: String) {
+        coroutineScope {
+            launch { planetsRemoteDataSource.deletePlanet(planetId) }
+            launch { planetsLocalDataSource.deletePlanet(planetId) }
+        }
+    }
 }
